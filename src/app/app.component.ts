69c65d79f02b0fe99a3357371fde52c1f99f1ddc
Ravi3599 <ravikantdukre2018@gmail.com>
2022-03-07 16:08:33 +0530

    UPDATED: while displaying message with user inputted name display validation error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ROUTER_CONFIGURATION } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent {
   url="http://www.bridgelabz.com";
 
   userName:string="";
+  nameError:string="";
 
   ngOnInit():void{
     this.title="Hello from BridgeLabz.";
@@ -21,5 +23,15 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is Clicked",$event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change Event Occured",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }
